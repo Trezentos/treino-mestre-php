18434bc5a0423c3305d5c967e8d1c5cbc7ed7d4e
Fagundes <gustavofagundes1998@hotmail.com>
2024-11-07 12:53:08 -0300

    last updats

--- validacao/registrar/registrar-controller.php
+++ validacao/registrar/registrar-controller.php
@@ -4,12 +4,14 @@
 
 // 2 Crie a classe de validacao
 
-// 4 Use a classe de validação 
+// 4 Use a classe de validação  [required, email, confirmed, min:8, max:30, strong]
 
 // 5 Se a validação não passar notifique o usuário
 
 // 6 Se passar, notifique o usuário que a validação passou
 
 // BORA
+
+
 
 ?>
